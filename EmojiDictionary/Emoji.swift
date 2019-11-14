7a1d255f2b0db9b43b2163c716dcc035b8a6b76a
Jason Liu <jliu@lcs.on.ca>
2019-11-13 21:01:27 -0500

    Creating the struct

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,21 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Liu, Jason on 2019-11-13.
+//  Copyright © 2019 Liu Jason. All rights reserved.
+//
+
+import Foundation
+
+//Defube tge enihu struct
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
+// An example of creating an instance of Emoji
+// var example = Emoji(symbol: "🍕", name: "Pizza", description: "Delicious combination of bread, tomato sauce, and cheese.", usage: "Make when hungry")
+//
